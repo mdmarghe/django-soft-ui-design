adb3aaccedeb16414f9f7294b1a195fa5e5f6529
mdmarghe <margheritanuccio98@gmail.com>
2023-08-31 20:38:06 +0200

    prova

--- home/models.py
+++ home/models.py
@@ -11,15 +11,25 @@ class Wine(models.Model):
     production_house = models.CharField(max_length=100)
     grape_variety = models.CharField(max_length=100)
     denomination_of_origin = models.CharField(max_length=100)
+    quantity_available = models.PositiveIntegerField(default=0)
     # Other fields as needed
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     wines = models.ManyToManyField(Wine, through='OrderedWine')
     order_date = models.DateTimeField(auto_now_add=True)
-    # Altri campi come stato dell'ordine, totale, ecc.
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+
 
 class OrderedWine(models.Model):
     wine = models.ForeignKey(Wine, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.wine.quantity_available -= self.quantity
+        self.wine.save()
+
+
